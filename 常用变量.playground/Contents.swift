c07d06b5f0faec045e652b53dc7800300e2a2571
胡双飞 <2868352478@qq.com>
2018-01-04 13:31:50 +0800

    常用变量var let

---
+++ 常用变量.playground/Contents.swift
@@ -0,0 +1,64 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//可改变的变量
+var str = "var to make a variable "
+
+//常量
+let letStr  = "let make a constant "
+
+//Int
+var myVariabel = 42
+myVariabel = 50
+print(Int.max)
+print(Int.min)
+
+print(myVariabel)
+
+//Int
+let myConstant = 20
+print(myConstant)
+
+//Double
+let implicitDouble = 70.0
+print(implicitDouble)
+
+//Float
+let float: Float = 70.0
+
+//Bool
+var boolVar: Bool = false
+print(boolVar)
+
+//String + Int
+let label = "The width is "
+let width = 50
+let widthLabel = label + String(width)
+print(widthLabel)
+
+//\() 输入内容
+let apples = 10
+let oranges = 5
+print("I have \(apples + oranges) piceces of fruit")
+
+
+// """
+// 这里输入多行的内容
+// """
+
+let quotation = """
+    Even though there's whitespace to the left,
+    the actual lines aren't indented.
+    这个时候，可以出现"" 。在多行的内里
+"""
+
+print(quotation)
+
+
+
+
+
+
+
+
+
